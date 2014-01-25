c9f72101d46919334f880a6389addbe9373b8ccc
Alexander Brade <Alexander.Brade@comsoft.aero>
2014-01-25 21:08:45 +0100

    Part VIII

--- app/views.py
+++ app/views.py
@@ -68,6 +68,8 @@ def after_login(resp):
         user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
         db.session.add(user)
         db.session.commit()
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
@@ -97,6 +99,44 @@ def user(nickname):
     return render_template('user.html',
                            user = user,
                            posts = posts);
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found!')
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t follower yourself!')
+        return redirect(url_for('index'))
+    u = g.user.followe(user)
+    if u is None:
+        flash('Cannot follow ' + nickname +'.')
+        return redirect(url_for('user', nickname = nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are followe ' + nickname + ' now.')
+    return redirect(url_for('user', nickname = nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found!')
+        return redirect(url_for('index'))
+    if user == g.user:
+        flash('You can\'t unfollow yourself!')
+        return redirect(url_for('user', nickname = nickname))
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname = nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('index'))
 
 @app.route('/edit', methods = ['GET', 'POST' ])
 @login_required
